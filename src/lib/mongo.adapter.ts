fdeaa063e387f25901f83b935ec437bb69b80328
TalkySafe143 <sebastian.galindo143@gmail.com>
2025-07-25 21:39:00 -0500

    Add: Format to all files

--- src/lib/mongo.adapter.ts
+++ src/lib/mongo.adapter.ts
@@ -2,29 +2,29 @@ import { IDatabase } from "./database.interface";
 import MongoDB from "./mongo";
 
 export class MongoAdapter implements IDatabase {
-  private db = MongoDB.getInstance();
+    private db = MongoDB.getInstance();
 
-  async insert<T>(collection: string, data: T) {
-    return this.db.insertDocument(collection, data);
-  }
+    async insert<T>(collection: string, data: T) {
+        return this.db.insertDocument(collection, data);
+    }
 
-  async insertMany<T>(collection: string, data: T[]) {
-    return this.db.insertManyDocuments(collection, data);
-  }
+    async insertMany<T>(collection: string, data: T[]) {
+        return this.db.insertManyDocuments(collection, data);
+    }
 
-  async getAll(collection: string) {
-    return this.db.getAllDocuments(collection);
-  }
+    async getAll(collection: string) {
+        return this.db.getAllDocuments(collection);
+    }
 
-  async getBy<T>(collection: string, query: Partial<T>) {
-    return await this.db.getOneDocument(collection, query);
-  }
+    async getBy<T>(collection: string, query: Partial<T>) {
+        return await this.db.getOneDocument(collection, query);
+    }
 
-  async update<T>(collection: string, query: Partial<T>, data: Partial<T>) {
-    return this.db.updateOneDocument(collection, query, data);
-  }
+    async update<T>(collection: string, query: Partial<T>, data: Partial<T>) {
+        return this.db.updateOneDocument(collection, query, data);
+    }
 
-  async delete<T>(collection: string, query: Partial<T>) {
-    return this.db.deleteOneDocument(collection, query);
-  }
+    async delete<T>(collection: string, query: Partial<T>) {
+        return this.db.deleteOneDocument(collection, query);
+    }
 }
