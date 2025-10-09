e9043c6c79c479abbc933f2bf973cee28683f75a
Sebastian Galindo <sebastian.galindo143@gmail.com>
2025-10-09 17:53:56 -0500

    Fix: Supabase decoded base 64

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -7,92 +7,100 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 export const checkJWTExp = (token: string): any | null => {
-  try {
-    return jwt.verify(token, Bun.env.JWT_SECRET);
-  } catch (e) {
-    return null;
-  }
+    try {
+        return jwt.verify(token, Bun.env.JWT_SECRET);
+    } catch (e) {
+        return null;
+    }
 };
 export const signJWT = (payload: any) => {
-  return jwt.sign(
-    {
-      // 1 hour
-      exp: Math.floor(Date.now() / 1000) + 60 * 60,
-      data: payload,
-    },
-    Bun.env.JWT_SECRET,
-  );
+    return jwt.sign(
+        {
+            // 1 hour
+            exp: Math.floor(Date.now() / 1000) + 60 * 60,
+            data: payload,
+        },
+        Bun.env.JWT_SECRET,
+    );
 };
 
 export const verifyJWT = async (context: Context) => {
-  const auth: string | null = context.request.headers.get("authorization");
-  if (!auth) return Unauthorized(context, "No Bearer JWT");
-  const [type, token] = auth.split(" ");
+    const auth: string | null = context.request.headers.get("authorization");
+    if (!auth) return Unauthorized(context, "No Bearer JWT");
+    const [type, token] = auth.split(" ");
 
-  if (type !== "Bearer") return Unauthorized(context, "Malformed Bearer token");
-  let decoded = null;
-  /*/ acm-auth-signed-supabase */
-  if( context.request.headers.get("acm-auth-signed-supabase")) {
-    decoded = await SupabaseDB.getInstance().verifySignedJWT(token)
-  }
-  else{
-    decoded = checkJWTExp(token);
-  }
-  if(!decoded) return Unauthorized(context, "Token isn't valid");
+    if (type !== "Bearer") return Unauthorized(context, "Malformed Bearer token");
+    let decoded = null;
+    /*/ acm-auth-signed-supabase */
+    if (context.request.headers.get("acm-auth-signed-supabase")) {
+        const [base_type, token_base_64] = token.split("-");
+        if (base_type === "base64") {
+            const token_str_json: string = atob(token_base_64); // decode base 64
+            const { access_token }: any = JSON.parse(token_str_json);
+            decoded = await SupabaseDB.getInstance().verifySignedJWT(access_token)
+            if (decoded != null) {
+                decoded = { email: decoded.email! };
+            }
+        }
+    }
+    else {
+        decoded = checkJWTExp(token);
+    }
+    if (!decoded) return Unauthorized(context, "Token isn't valid");
 
-  context.store.user = decoded;
+    context.store.user = decoded;
 };
 
 export const checkBasicAuth = async (context: Context, cb: Function) => {
-  const auth: string | null = context.request.headers.get("authorization");
-  if (!auth || auth.split(" ")[0] != "Basic")
-    return BadRequest(
-      context,
-      "Auth header do not exist or malformed Basic auth header",
-    );
-  const token = auth.split(" ")[1];
-  const [user, password] = Buffer.from(token, "base64")
-    .toString("binary")
-    .split(":");
+    const auth: string | null = context.request.headers.get("authorization");
+    if (!auth || auth.split(" ")[0] != "Basic")
+        return BadRequest(
+            context,
+            "Auth header do not exist or malformed Basic auth header",
+        );
+    const token = auth.split(" ")[1];
+    const [user, password] = Buffer.from(token, "base64")
+        .toString("binary")
+        .split(":");
 
-  if (!(await cb(user, password)))
-    return BadRequest(context, "Wrong credentials");
+    if (!(await cb(user, password)))
+        return BadRequest(context, "Wrong credentials");
 
-  return {
-    success: true,
-    user,
-  };
+    return {
+        success: true,
+        user,
+    };
 };
 
 const superUserCallback = (
-  username: string,
-  password: string,
+    username: string,
+    password: string,
 ): Promise<boolean> => {
-  return new Promise((resolve, _) => {
-    resolve(
-      username == Bun.env.USER_MASTER && password == Bun.env.PASSWORD_MASTER,
-    );
-  });
+    return new Promise((resolve, _) => {
+        resolve(
+            username == Bun.env.USER_MASTER && password == Bun.env.PASSWORD_MASTER,
+        );
+    });
 };
 
 const normalUserCallback = async (
-  email: string,
-  password: string,
+    email: string,
+    password: string,
 ): Promise<boolean> => {
-  const db: IDatabase = new MongoAdapter();
-  const result = await db.getBy("users", { email });
+    const db: IDatabase = new MongoAdapter();
+    const result = await db.getBy("users", { email });
 
-  if (result.error) return false;
+    if (result.error) return false;
 
-  return bcrypt.compareSync(password, result.data.password);
+    return bcrypt.compareSync(password, result.data.password);
 };
 
 export const checkSignIn = async (context: Context) => {
-  let res = await checkBasicAuth(context, superUserCallback);
-  if (!res.success) res = await checkBasicAuth(context, normalUserCallback);
-  if (!res.success) return res;
-  context.store.user = {
-    email: res.user,
-    super: res.user === Bun.env.USER_MASTER,
-  };
+    let res = await checkBasicAuth(context, superUserCallback);
+    if (!res.success) res = await checkBasicAuth(context, normalUserCallback);
+    if (!res.success) return res;
+    context.store.user = {
+        email: res.user,
+        super: res.user === Bun.env.USER_MASTER,
+    };
 };
