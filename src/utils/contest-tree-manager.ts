bae5d2fc217cd8b125624829b4f2f7ed4b2b75fe
SalomonAvila <avila_sa@javeriana.edu.co>
2026-01-15 22:25:31 -0500

    Fix: fixed matchmaking tree

--- src/utils/contest-tree-manager.ts
+++ src/utils/contest-tree-manager.ts
@@ -2,31 +2,35 @@ import { IDatabase } from "../db/database.interface";
 import { MongoAdapter } from "../db/mongo/mongo.adapter";
 import { MatchmakingTreeNode } from "./matchmaking-tree";
 
-const COLLECTION = "contest_trees";
+const COLLECTION = "matchmaking";
 const db: IDatabase = new MongoAdapter();
 
 export var catcheContestTree: Map<number, MatchmakingTreeNode> = new Map();
 
-export function getTreeByContestId(contestId: number){
-    if (catcheContestTree.has(contestId)){
-        return catcheContestTree.get(contestId)!;
+export async function getTreeByContestId(contestId: number) {
+    if (catcheContestTree.has(contestId)) {
+        const node = catcheContestTree.get(contestId)!;
+        // Re-insertar para moverla al final (más recientemente usada)
+        catcheContestTree.delete(contestId);
+        catcheContestTree.set(contestId, node);
+        return node;
     }
     else {
-        db.getBy(COLLECTION, {contestId: contestId}).then(result => {
-            if (!result.error && result.data.length > 0){
-                const treeData = result.data[0];
-                const rootNode = treeData as MatchmakingTreeNode;
-                catcheContestTree.set(contestId, rootNode);
-                if (catcheContestTree.size > 5){
-                    // Delete oldest cache
-                    const firstKey = catcheContestTree.keys().next().value;
-                    if (firstKey !== undefined)
-                        catcheContestTree.delete(firstKey);
-                }
-                return rootNode;
-            } else {
-                return null;
+        const result = await db.getBy(COLLECTION, { contestId: contestId });
+        if (!result.error && result.data.length > 0) {
+            const treeData = result.data[0].tree;
+            const rootNode = treeData as MatchmakingTreeNode;
+            catcheContestTree.set(contestId, rootNode);
+            if (catcheContestTree.size > 5) {
+                // Delete oldest cache
+                const firstKey = catcheContestTree.keys().next().value;
+                if (firstKey !== undefined)
+                    catcheContestTree.delete(firstKey);
             }
-        });
+            return rootNode;
+        } else {
+            return null;
+        }
+
     }
 }
